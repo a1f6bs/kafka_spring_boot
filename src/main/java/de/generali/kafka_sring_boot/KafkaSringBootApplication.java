6053e59043678623ebd465a1730e519d8489f99b
Stephan Brockert <35650450+a1f6bs@users.noreply.github.com>
2023-02-27 15:02:55 +0100

    Add files via upload
    initial commit

---
+++ src/main/java/de/generali/kafka_sring_boot/KafkaSringBootApplication.java
@@ -0,0 +1,13 @@
+package de.generali.kafka_sring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/generali/kafka_sring_boot/KafkaSringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.generali.kafka_sring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
